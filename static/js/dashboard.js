70e31093a359741152c4e9f86010a1a18669ceb4
Jenna Antilla <jennantilla@gmail.com>
2019-11-26 02:20:20 +0000

    implement AJAX for appendTo #inactive

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -14,28 +14,3 @@ $("#performance").on('submit', (evt) => {
     $("#question").addClass('hide');    
 
     });
-
-
-
-
-
-// $("#deactivate").on("submit", (evt) => {
-//     evt.preventDefault();
-
-//     const formAnswer = $('#remove').serialize();
-//     $.post('/dashboard/1', formAnswer);
-
-//     $("#routine-item").appendTo('#inactive');
-
-
-    // find the list of active stuff
-
-    // find the elemtn we want to remove in that list
-
-    // find the list of inactive stuff
-    // add the elemebnt to the list of inactive 
-    // remove the element from the list of active
-
-
-
-    // });
