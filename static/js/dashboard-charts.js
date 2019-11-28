5dc2897951bb7f14bc534c4317c528ec1ce4cf4f
Jenna Antilla <jennantilla@gmail.com>
2019-11-28 09:33:39 +0000

    Refactor dashboard and create SQL dump file of database

--- static/js/dashboard-charts.js
+++ static/js/dashboard-charts.js
@@ -1,24 +1,4 @@
 "use strict";
-
-const options = {
-    responsive: true
-};
-
-let ctx_donut = $("#productTypeChart").get(0).getContext("2d");
-
-$.get("/product-type.json", function (data) {
-    let myProductChart = new Chart(ctx_donut, {
-                                        type: "pie",
-                                        data: data,
-                                        options: options
-                                        });
-    });
-
-
-
-
-
-
 Chart.pluginService.register({
   beforeDraw: function(chart) {
     var width = chart.chart.width,
